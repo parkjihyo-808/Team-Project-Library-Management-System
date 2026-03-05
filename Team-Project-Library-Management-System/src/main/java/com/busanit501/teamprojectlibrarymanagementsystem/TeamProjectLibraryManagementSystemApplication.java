fcfe3a68b0ab7f5413f89abff1e75fbbb14727ce
parkjihyo <wlgy5678@gmail.com>
2026-03-05 16:40:42 +0900

    1

---
+++ Team-Project-Library-Management-System/src/main/java/com/busanit501/teamprojectlibrarymanagementsystem/TeamProjectLibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.busanit501.teamprojectlibrarymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectLibraryManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProjectLibraryManagementSystemApplication.class, args);
+    }
+
+}
---
+++ Team-Project-Library-Management-System/src/test/java/com/busanit501/teamprojectlibrarymanagementsystem/TeamProjectLibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busanit501.teamprojectlibrarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectLibraryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
